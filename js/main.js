7d6aa3a268c9cae9069c2eb077ff610f2ea75310
Brymo <bryanmoh@yahoo.com.au>
2019-09-14 14:40:31 +1000

    Bout to checkout

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-grabFile =  async (index, folderName) => {
+grabFile =  async () => {
     const Octokit = require('@octokit/rest');
     const octokit = new Octokit();
     const stuff = await octokit.repos.getContents({
       owner:"Brymo",
       repo:"TwentySeconds",
-      path:`./src/blogs/${folderName}/${folderName}${index}.md`
+      path:`./Posts/Bryan/message.txt`
     })
     const content = await Buffer.from(stuff.data.content,'Base64').toString();
     return content;
-  }
+}
+
+
+const p = new Promise((resolve,reject) => {
+    const text = grabFile()
+    resolve(text);
+});
+
+p.then( (result) => {
+    document.write(result);
+});
