46b056dec05423acc4dbcb4e34b18d3eeeac9ba4
Brymo <bryanmoh@yahoo.com.au>
2019-09-15 21:26:39 +1000

    now only looks at current main repo

--- js/bundle.js
+++ js/bundle.js
@@ -20,7 +20,17 @@ grabFile =  async (user) => {
 });*/
 
 const axios = require('axios');
-axios.get('https://api.github.com/repos/Brymo/TwentySeconds/git/trees/02e3aa84e08c43c5ae0a1942ca161bbf782b919f')
+axios.get('https://api.github.com/repos/Brymo/TwentySeconds/git/trees/master')
+.then((result) => {
+    
+    const objects = result.data.tree;
+    const sha = objects.filter( (item) => item.path === "Posts");
+    
+    return sha[0].sha;
+
+}).then((result) => {
+    return axios.get(`https://api.github.com/repos/Brymo/TwentySeconds/git/trees/${result}`)
+})
 .then(
     (result) => {
         const treeData = result.data.tree;
--- js/main.js
+++ js/main.js
@@ -18,7 +18,17 @@ grabFile =  async (user) => {
 });*/
 
 const axios = require('axios');
-axios.get('https://api.github.com/repos/Brymo/TwentySeconds/git/trees/5309173b8271c4def4c3458ead1a9714f8a4a4f0')
+axios.get('https://api.github.com/repos/Brymo/TwentySeconds/git/trees/master')
+.then((result) => {
+    
+    const objects = result.data.tree;
+    const sha = objects.filter( (item) => item.path === "Posts");
+    
+    return sha[0].sha;
+
+}).then((result) => {
+    return axios.get(`https://api.github.com/repos/Brymo/TwentySeconds/git/trees/${result}`)
+})
 .then(
     (result) => {
         const treeData = result.data.tree;
