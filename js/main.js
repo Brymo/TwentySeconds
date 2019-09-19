1697000cd78a6ca311413c882bd5d048e67d583b
Brymo <bryanmoh@yahoo.com.au>
2019-09-20 00:17:09 +1000

    Hopefully not the final

--- js/main.js
+++ js/main.js
@@ -12,29 +12,59 @@ grabFile =  async (user) => {
 }
 
 
-const axios = require('axios');
-axios.get('https://www.plainlaundry.com')
-.then((result) => {
-    const data = result.data.wishers;
-    const wishers = Object.keys(data);
+function getRequests(){
+    const axios = require('axios');
+    axios.get('https://www.plainlaundry.com')
+    .then((result) => {
+        const data = result.data.wishers;
+        const wishers = Object.keys(data);
 
-    const wisherData = wishers.map((name) => {
-        return {name: name, wish: data[name]}
-    })
-    
-    decoratePage(wisherData);
-
-})
-
-
-function decoratePage(wisherData){
-
-    wisherData.forEach(element => {
+        const wisherData = wishers.map((name) => {
+            return {name: name, wish: data[name]}
+        })
         
-        const main = document.getElementById("mainframe");
-        const newElem = document.createElement('div');
-        newElem.innerHTML = element.name +": "+ element.wish;
-        main.appendChild(newElem);
-    });
+        const maxImages = 10;
+        const cdnURL = "https://d3ccubkld17jql.cloudfront.net/"
+        let lrtoggle = true;
+        let imgCount = 0;
+        wisherData.forEach(element => {
+        
+            const main = document.querySelector("#root");
+            const newMessage = document.createElement('div');
+            const newText = document.createElement("div");
 
+            newText.innerHTML = element.wish +" - "+element.name;
+
+            const newImg = document.createElement("img");
+
+            newImg.src =  cdnURL+((imgCount++ % maxImages)+1);
+
+            if(lrtoggle){
+                newText.className = "msgtxtl";
+                newImg.className = "msgimgr";
+                newMessage.className = "messageframel";
+                newMessage.appendChild(newImg);
+                newMessage.appendChild(newText);
+            }else{
+                newText.className = "msgtxtr";
+                newImg.className = "msgimgl";
+                newMessage.className = "messageframer";
+                newMessage.appendChild(newText);
+                newMessage.appendChild(newImg);
+            }
+            lrtoggle = !lrtoggle;
+
+            //newElem.innerHTML = element.name +": "+ element.wish;
+            main.appendChild(newMessage);
+        });
+
+    })
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+const mainframe = document.getElementById("mainframe");
+mainframe.style.height = window.innerHeight + "px";
+getRequests();
